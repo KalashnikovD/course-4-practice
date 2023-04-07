ccb116d6edd4881c1dadebb49d7ee15451f96a5b
KalashnikovD <kalashnikov.dmytro@gmail.com>
2023-04-07 16:54:30 +0200

    add webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './css/normalize.scss';
+import './css/style.scss';
+import logo from './media/logo.png'
+import promotion from './media/promotion.jpg'
+import blogImage1 from './media/blog-image-1.jpg'
+import blogImage2 from './media/blog-image-2.jpg'
+import blogImage3 from './media/blog-image-3.jpg'
+import logoFooter from './media/logo-footer.png'
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './built'),
+        filename: '[name].bundle.js',
+        assetModuleFilename: path.join('media', '[name].[contenthash][ext]'),
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ],
+                plugins: ['@babel/plugin-proposal-class-properties']
+              }
+            }
+          },
+          {
+            test: /\.(scss|css)$/,            
+            use: [ MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('media/', '[name][ext]'),
+                },
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.join('icons', '[name].[ext]'),
+                },
+            },
+        ]
+      },
+
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+                 filename: '[name].[contenthash].css',
+         }),
+    ]
+}
